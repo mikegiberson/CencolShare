0f44cd000036f00d3baa19e55e522f4244fa671a
TramDo <dothuytram@gmail.com>
2014-03-12 09:38:46 -0400

    no message

--- src/main/java/com/cencolshare/controller/DocumentController.java
+++ src/main/java/com/cencolshare/controller/DocumentController.java
@@ -1,17 +1,31 @@
 package com.cencolshare.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cencolshare.service.DocumentService;
+
 @Controller
 @RequestMapping("/docs")
 public class DocumentController {
 	
-	@RequestMapping(value={"", "index"}, method=RequestMethod.GET)
+	@Autowired
+	DocumentService documentService;
+	
+	@RequestMapping(value={"/list"}, method=RequestMethod.GET)
 	public ModelAndView showDashboard() {
-		ModelAndView mav = new ModelAndView("docs/index");
+		ModelAndView mav = new ModelAndView("docs/list");
+		
+		mav.addObject("documentList",documentService.findAll());
+		return mav;
+	}
+		
+	@RequestMapping(value="/upload", method=RequestMethod.GET)
+	public ModelAndView uploadDocs() {
+		ModelAndView mav = new ModelAndView("docs/document-upload");
 		return mav;
 	}
 
--- src/main/webapp/WEB-INF/views/docs/index.jsp
+++ src/main/webapp/WEB-INF/views/docs/index.jsp
@@ -2,6 +2,7 @@
 <%@ include file="../common/sidebar.jsp"%>    
         <div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-2 main">
           <h1 class="page-header">My Docs</h1>
+          
             
           <div class="row">
             <div class="col-md-4">
@@ -17,6 +18,7 @@
                   </div>
                 </div>
             </div>
+           
             <div class="col-md-4">
                   <div class="thumbnail">
                   <br />
