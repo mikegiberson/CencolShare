d81936221e0be674e0db66a14de5a535b71def4a
Lovu Dhiraj Sharma <lovudsharma@gmail.com>
2014-03-17 15:56:19 -0400

    Test cases for All methods in GroupService

--- src/test/java/com/cencolshare/data/MockData.java
+++ src/test/java/com/cencolshare/data/MockData.java
@@ -1,10 +1,10 @@
 package com.cencolshare.data;
 
-import java.util.Date;
-
 import org.springframework.stereotype.Component;
 
+import com.cencolshare.enums.Role;
 import com.cencolshare.model.Discussion;
+import com.cencolshare.model.Group;
 import com.cencolshare.model.User;
 import com.github.javafaker.Faker;
 
@@ -19,9 +19,11 @@ public class MockData {
 		user.setLastName(faker.lastName());
 		user.setPassword(faker.firstName() + "password");
 		user.setEmail(faker.firstName() + "@gmail.com");
+		user.setRole(Role.USER);
 		user.setEnabled(true);
 		return user;
 	}
+	
 	
 	public Discussion createDiscussion(User user) {
 		final Discussion discussion = new Discussion();
@@ -31,4 +33,11 @@ public class MockData {
 		return discussion;
 	}
 	
+	public Group createGroup() {
+		final Group group = new Group();
+		group.setGroupName(faker.name());
+		group.setGroupDescription(faker.paragraph(2));
+		group.setGroupImage("/cencolshare/resources/images/pdf.jpg");
+		return group;
+	}
 }
